6975ef50ceffda9e17805aff5db21bcd74280029
Raymond Toy <toy.raymond@gmail.com>
2026-05-16 07:41:55 -0700

    Fix #84:  Add regression test for MINPACK
    Integrate the MINPACK test programs into f2cl test suite.  This is
    kept separate from the main f2cl test suite.
    
    First fix is that in MINPACK, the test programs contain multiple
    subprograms and f2cl was erroneously changing the package to print out
    the finfo block.  This isn't needed, so remove it.  All of the
    generated code assumes that the CL package is being used so normal CL
    symbols work as expected.

--- src/f2cl1.lisp
+++ src/f2cl1.lisp
@@ -1286,11 +1286,10 @@ COMPILE-FILE.
        (setf fun (fixup-f2cl-lib fun (cons (cadr fort-fun) *external-function-names*)))
 
        (special-print fun outport)
-       ;; Dump out the function info this function.  We need to be in
-       ;; the CL-USER package.
-       (format outport "~2&(in-package #:cl-user)~%")
+       ;; Dump out the function info this function.
+       ;;
        ;; Only load the function info if the F2CL package exists.
-       (format outport "#+#.(cl:if (cl:find-package '#:f2cl) '(and) '(or))~%")
+       (format outport "~2&#+#.(cl:if (cl:find-package '#:f2cl) '(and) '(or))~%")
        ;; Write out the sexp that sets up the function info database
        ;; for this function.
        (let* ((*package* (find-package '#:cl-user))
